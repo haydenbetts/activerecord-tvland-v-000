7deca184795003d09e479483c53b70164dac4448
Hayden Betts <haydenbetts@gmail.com>
2018-07-13 18:06:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180713180439) do
+ActiveRecord::Schema.define(version: 20180713180558) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180713180439) do
     t.integer "network_id"
     t.string "day"
     t.string "genre"
+    t.string "season"
   end
 
 end
